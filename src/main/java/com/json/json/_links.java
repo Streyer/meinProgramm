2789005177c4b0531203b2391aa62495a4d6d729
Max Greis <max.greis90@gmail.com>
2018-04-22 21:53:25 +0200

    es wird jetzt durch alle Waffen+Patterns+Wears durch ittertiert

--- src/main/java/com/json/json/_links.java
+++ src/main/java/com/json/json/_links.java
@@ -1,7 +1,6 @@
 package com.json.json;
 
 
-
 public class _links {
     public static String[] weapons = new String[]{
             "http://steamcommunity.com/market/priceoverview/?appid=730&currency=3&market_hash_name=AK-47 | Safari Mesh (Field-Tested)",
@@ -12,15 +11,22 @@ public class _links {
 
 
     public static String[] getWeaponsIterateThrough() {
-        String[] weapons = new String[Weapons.weaponsList.length];
-        System.out.println(Weapons.weaponsList.length);
+        //String[] weapons = new String[Weapons.weaponsList.length];
+        String[] weapons = new String[Weapons.weaponsList.length * Weapons.weaponPatterns.length * Weapons.weaponsWear.length];
+        System.out.println(weapons.length);
 
+        int i = 0;
         for (int counter = 0; counter < Weapons.weaponsList.length; counter++) {
+            //System.out.println("Der Counter der WaffenListe ist bei: "+counter);
             for (int counter2 = 0; counter2 < Weapons.weaponPatterns.length; counter2++) {
-            weapons[counter] = "http://steamcommunity.com/market/priceoverview/?appid=730&currency=3&market_hash_name=" + Weapons.weaponsList[counter] + " | " + Weapons.weaponPatterns[counter2] + " (Field-Tested)";
+                for (int counter3 = 0; counter3 < Weapons.weaponsWear.length; counter3++) {
 
-            //System.out.println(weapons[counter]);
-        }}
+                    weapons[i] = "http://steamcommunity.com/market/priceoverview/?appid=730&currency=3&market_hash_name=" + Weapons.weaponsList[counter] + " | " + Weapons.weaponPatterns[counter2] + " " + Weapons.weaponsWear[counter3];
+                    //System.out.println("Der Counter der WaffenPatterns ist bei:" + counter2);
+                    i++;
+                }
+            }
+        }
 
 
         return weapons;
